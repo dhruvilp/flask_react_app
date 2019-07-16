81b1c41cf464d1b9231070890fa9ec6935425225
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-07-15 22:40:05 -0400

    tooltip

--- app/src/pages/TableButtons/TableButtons.js
+++ app/src/pages/TableButtons/TableButtons.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Tooltip } from 'reactstrap';
 import './TableButtons.css';
 
 class TableButtons extends Component{
     constructor() {
         super();
         this.state = {
-            data: []
+            data: ["button1","button2","button3","button4","button5","button6"],
+            tooltipOpen: false
         };
-      }
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+          tooltipOpen: !this.state.tooltipOpen
+        });
+    }
     
     render(){
         return(
@@ -27,24 +36,14 @@ class TableButtons extends Component{
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
+                        {this.state.data.map(val => 
                         <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
+                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0" id={val}>{val}</button>
+                            <Tooltip placement="top" isOpen={this.state.tooltipOpen} autohide={false} target={val} toggle={this.toggle}>
+                                Try to select {val} text!
+                            </Tooltip>
                         </td>
-                        <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-teal btn-rounded btn-sm m-0">Button</button>
-                        </td>
+                        )}
                     </tr>
                     <tr>
                         <th scope="row">2</th>
